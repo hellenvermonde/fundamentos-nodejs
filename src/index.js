d91d12c0c33d1907217a456ef43ea307389ba8cd
Hellen Vermonde <hellenvermonde@gmail.com>
2022-09-24 18:54:44 -0300

    projeto fundamentos nodejs

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response)=> {
+    // return response.send("Hello Word") //Utilizado método send -> retorna um Texto 
+    return response.json({message: "Hello word! Ignite"}) //Utilizando método json -> retorna um dado 
+})
+
+
+app.get("/courses", (request, response)=>{
+    const query = request.query;
+    console.log(query)
+
+    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+})
+
+app.post("/courses", (request, response)=>{
+    const body = request.body
+    console.log(body)
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.put("/courses/:id", (request, response)=>{
+    const { id } = request.params;
+    console.log(id)
+
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.patch("/courses/:id", (request, response)=>{
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.delete("/courses/:id", (request, response)=>{
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.listen(3333); //Definir porta e inicializar aplicação
